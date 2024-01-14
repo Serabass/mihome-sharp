8183cade2fbb8e735400f06ec5b8c45ecb89d007
Serabass <serabass565@gmail.com>
2024-01-14 23:30:00 +0600

    upd

--- MihomeCLI/Ext.cs
+++ MihomeCLI/Ext.cs
@@ -2,10 +2,14 @@
 
 public class Ext
 {
-  public string GenerateRandomString(int length, string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-  {
-    var random = new Random();
-    return new string(Enumerable.Repeat(chars, length)
-      .Select(s => s[random.Next(s.Length)]).ToArray());
-  }
+    public string GenerateRandomString(
+        int length,
+        string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    )
+    {
+        var random = new Random();
+        return new string(
+            Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray()
+        );
+    }
 }
